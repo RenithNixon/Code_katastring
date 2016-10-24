d620457a1857d0d560a842a45da96e6854bdcc27
RenithNixon <renithreni95@gmail.com>
2016-10-24 10:11:30 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class StringToInt
+{
+public static void main(String args[])
+{
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+int n=Integer.parseInt(s);
+System.out.println("The number is:"+n);
+}
+}
